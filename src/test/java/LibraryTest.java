880597fcaa303ac5446b5191d15718c3d8639a0f
Merrick MacInnis <merrickmac06@gmail.com>
2025-05-08 14:19:17 -0400

    Added unit testing for the signUp method in the Library class

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,5 @@
 import org.example.*;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,14 @@ import java.util.List;
 import java.util.Map;
 
 public class LibraryTest {
+
+    @AfterEach
+    public void resetCounts() {
+        User.idCount = 1;
+        Media.callCount = 1;
+    }
+
+    //checkOut Tests
 
     @Test
     public void checkOut_Test1() {
@@ -73,5 +82,42 @@ public class LibraryTest {
                 () -> Assertions.assertEquals(l1.loans, e1, "Checking if both loan maps are the same")
         );
 
+    }
+
+    //signUp Tests
+
+    @Test
+    public void signUp_Test1() {
+        Library l1 = new Library();
+
+        List<User> u = new LinkedList<>();
+
+        l1.signUp("waidhawd", "Bob");
+        Assertions.assertEquals(u, l1.users);
+    }
+
+    @Test
+    public void signUp_Test2() {
+        Library l1 = new Library();
+        l1.signUp("wow@wow.com", "Bob");
+
+        List<User> u = new LinkedList<>();
+        User u1 = new Patron("Bob", 0, "wow@wow.com");
+        u1.setId(1);
+        u.add(u1);
+
+        Assertions.assertEquals(u, l1.users);
+    }
+    @Test
+    public void signUp_Test3() {
+        Library l1 = new Library();
+        User u1 = new Patron("Bob", 0, "wow@wow.com");
+        l1.users.add(u1);
+        l1.signUp("wow@wow.com", "Carl");
+
+        List<User> u = new LinkedList<>();
+        u.add(u1);
+
+        Assertions.assertEquals(u, l1.users);
     }
 }
